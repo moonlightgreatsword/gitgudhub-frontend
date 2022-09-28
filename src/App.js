818def57209d38f87a25e0c7f80050e5d778dfb0
alissonkhr <alissonricardo68@gmail.com>
2022-09-27 17:48:03 -0700

    pushing a test change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+//checking if changes to branch worked -Alisson
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -15,28 +17,40 @@ class App extends Component {
       gamesQuery: "search=",
       searchInput: "",
       // later on we can determine whether to add additional parameters like page size, genres, etc.
-      searchURL: ""
+      searchURL: "",
     };
-  };
+  }
   handleChange = (event) => {
     this.setState({
       // we're grabbing the element or elements and dynamically setting the input value to the key corresponding to the input id of the same name in this.state
-      [event.target.id]: event.target.value
+      [event.target.id]: event.target.value,
     });
   };
   handleSubmit = (event) => {
     // keep the page from refreshing on submit
     event.preventDefault();
-    this.setState({
-      // builds out our search url from the pieces we've assembled
-      searchURL: this.state.baseURL + this.state.apiKey + this.state.gamesQuery + this.state.searchInput
-    }, () => {
-      // we fetch the url from the api
-      fetch(this.state.searchURL)
-      // .then waits till the fetch is complete
-      .then(response => {return response.json()})
-      .then(json => console.log(json), (err) => console.log(err))
-    })
+    this.setState(
+      {
+        // builds out our search url from the pieces we've assembled
+        searchURL:
+          this.state.baseURL +
+          this.state.apiKey +
+          this.state.gamesQuery +
+          this.state.searchInput,
+      },
+      () => {
+        // we fetch the url from the api
+        fetch(this.state.searchURL)
+          // .then waits till the fetch is complete
+          .then((response) => {
+            return response.json();
+          })
+          .then(
+            (json) => console.log(json),
+            (err) => console.log(err)
+          );
+      }
+    );
   };
   render() {
     return (
@@ -50,14 +64,11 @@ class App extends Component {
             value={this.state.searchInput}
             onChange={this.handleChange}
           />
-          <input
-            type="submit"
-            value="Find Games"
-          />
+          <input type="submit" value="Find Games" />
         </form>
       </div>
     );
-  };
-};
+  }
+}
 
 export default App;
