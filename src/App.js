35e67e1ab15930aa152be1f7e71797f6a8096a33
harrisv25 <harrisv25@gmail.com>
2022-09-28 18:39:44 -0600

    Changed the app.js to Search.js. Created new cleaner app.js. In search.js, just added a button for testing purposes.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import React, { Component } from "react";
+import logo from './logo.svg';
+import './App.css';
+import ReactDOM from 'react-dom/client'
+import Search from './Search'
 
 // linking to backend here vvvv
 let baseURL = "";
@@ -9,83 +12,14 @@ if (process.env.NODE_ENV === "development") {
   baseURL = "your heroku backend url here";
 }
 
-console.log("current base URL:", baseURL);
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      /*
-      API request format:
-      GET https://api.rawg.io/api/platforms?key=YOUR_API_KEY
-      GET https://api.rawg.io/api/games?key=YOUR_API_KEY&dates=2019-09-01,2019-09-30&platforms=18,1,7
-      Docs: https://api.rawg.io/docs
-      */
-      baseURL: "https://api.rawg.io/api/games?",
-      apiKey: `key=${process.env.REACT_APP_RAWG_API_KEY}&`,
-      gamesQuery: "search=",
-      searchInput: "",
-      // later on we can determine whether to add additional parameters like page size, genres, etc.
-      searchURL: "",
-      gallery : []
-    };
-  }
-  handleChange = (event) => {
-    this.setState({
-      // we're grabbing the element or elements and dynamically setting the input value to the key corresponding to the input id of the same name in this.state
-      [event.target.id]: event.target.value,
-    });
-  };
-  handleSubmit = (event) => {
-    // keep the page from refreshing on submit
-    event.preventDefault();
-    this.setState(
-      {
-        // builds out our search url from the pieces we've assembled
-        searchURL:
-          this.state.baseURL +
-          this.state.apiKey +
-          this.state.gamesQuery +
-          this.state.searchInput,
-      },
-      () => {
-        // we fetch the url from the api
-        fetch(this.state.searchURL)
-          // .then waits till the fetch is complete
-          .then((response) => {
-            return response.json();
-          })
-          .then(
-            (json) => this.setState({
-              gallery : json.results
-            }),
-            (err) => console.log(err)
-          );
-      }
-    );
-  };
-  displayGallery = (gallery) => {
-  }
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <label>Search</label>
-          <input
-            id="searchInput"
-            type="text"
-            placeholder="What's the Name of the Game"
-            value={this.state.searchInput}
-            onChange={this.handleChange}
-          />
-          <input type="submit" value="Find Games" />
-        </form>
-        <div id='gallery'>
-          {this.state.gallery.map(function(d, idx){return (<li key={idx}>{d.name}</li>)})}
-        </div>
+function App() {
+  return (
+    <div className="App">
+      <div id="root">
+        <Search />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
