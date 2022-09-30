e4706837726699a18782bb6f020627aa492c2cae
harrisv25 <harrisv25@gmail.com>
2022-09-30 17:49:31 -0600

    can now display pages based on routes. added navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import ReactDOM from 'react-dom/client'
 import Search from './Search'
+import { Route, Routes, Link, Router } from 'react-router-dom';
+import NavBar from './NavBar'
 
 // linking to backend here vvvv
 let baseURL = "";
@@ -13,10 +15,16 @@ if (process.env.NODE_ENV === "development") {
 }
 
 function App() {
+
   return (
     <div className="App">
       <div id="root">
-        <Search />
+        <NavBar />
+        <Routes>
+          <Route exact path="/" element={<Search />} />
+          {/* <Router path='/:selected' component={SelectedGame} />
+          <Router exact  path='/reviewed' component={Reviewed} /> */}
+        </Routes>
       </div>
     </div>
   );
