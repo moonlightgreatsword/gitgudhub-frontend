2d0419b8e891b3a41e03b7b5b81a42d35bf8536f
msuh <suh.mikep@gmail.com>
2022-10-08 09:22:37 -0500

    removed import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import ReactDOM from 'react-dom/client'
-import Nav from './Nav'
 import Search from './Search'
 import { Route, Routes, Link, Router } from 'react-router-dom';
 import NavBar from './NavBar'
